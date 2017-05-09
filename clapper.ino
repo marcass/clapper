42791807a0117e5bbb941c075aed5870eb1d37a4
Marcus <best.nose@gmail.com>
2017-05-09 15:40:43 +1200

    refined code

--- clapper.ino
+++ clapper.ino
@@ -1,19 +1,24 @@
 const int relay = 2;
 const int soundPin = A0;
 int state;
-int clap;
-const int clapThresh = 40;
+int sound;
+const int clapThresh = 2;
 bool light;
-const long gap = 50; // puase to not count echo
+const long gap = 40; // puase to not count echo
 const long quiet = 200; //period that must be quiet to distiguish between clap and noise
 const long pause = 2000;
-unsigned long startTime;
+unsigned long startTime; //time for sequence of claps
+unsigned long clapTime; //time for each clap
+
+#define debug
 
 //states
 const int idle = 0;
-const int clap1  = 1;
-const int wait = 2;
-const int switching = 3;
+const int clapCheck = 1;
+const int clap1 = 2;
+const int clap2 = 3;
+const int switching = 4;
+int nextState;
 
 void setup() {
   Serial.begin(9600);
@@ -36,45 +41,79 @@ void toggle() {
   }
 }
 
+void clap_detect(int target_state){
+  //a clap is a short loud sound followed by no sound
+  if (millis()- clapTime > gap){ //wipe effect of echoes
+    if (sound > clapThresh) { //if clap happens in quite time go back to idle
+      delay(1000);
+      startTime = 0;
+      clapTime = 0;
+      state = idle;
+      #ifdef debug
+        Serial.println("Sound in gap -> idle");
+      #endif
+    }
+    if (millis() - clapTime > quiet) {
+      state = target_state;
+      #ifdef debug
+        Serial.print("Quiet for long enough, migrating to state ");
+        Serial.println(target_state);
+      #endif
+    }
+  }
+}
+
 void loop() {
-  clap = analogRead(soundPin);
+  sound = analogRead(soundPin);
   switch (state) {
     case idle:
-      if (clap > clapThresh) {
-        state = wait;
+      if (sound > clapThresh){
         startTime = millis();
+        clapTime = millis();
+        state = clapCheck;
+        nextState = clap1;
+        #ifdef debug
+          Serial.print("Sound heard, going to ");
+          Serial.println(state);
+        #endif
       }
       break;
-    case wait:
-      if (millis()- startTime > gap) {
-        if (clap > clapThresh) {
-          state = idle;
-        }
-        if (millis() - startTime > quiet) {
-          state = clap1;
-          startTime = millis();
-        }
-      }
+    case clapCheck:
+      clap_detect(nextState);
       break;
     case clap1:
-      if (millis()- startTime > pause) {
+      if (millis() - startTime > pause) {
         state = idle;
+        #ifdef debug
+          Serial.print("No additional clap, going to ");
+          Serial.println(state);
+        #endif
       }
-      if (clap > clapThresh) {
-        state = switching;
+      if (sound > clapThresh){
+        clapTime = millis();
+        state = clapCheck;
+        nextState = switching;
+        #ifdef debug
+          Serial.print("Sound heard, going to ");
+          Serial.println(state);
+        #endif
       }
       break;
     case switching:
       light = !light;      
       toggle();
       state = idle;
+      startTime = 0;
+      clapTime = 0;
       break;
   }
-  if (clap > clapThresh) {
-    Serial.print("Clap = ");
-    Serial.print(clap);
-    Serial.print("  State = ");
-    Serial.println(state);
-  }
+  #ifdef debug
+    if (sound > clapThresh) {
+      Serial.print("Sound = ");
+      Serial.print(sound);
+      Serial.print("  State = ");
+      Serial.println(state);
+    }
+  #endif
 }
       
